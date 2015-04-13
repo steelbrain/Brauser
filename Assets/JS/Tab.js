8b04f7a3a36b501409f29edcad17fc8e14b10327
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-13 16:00:31 -0700

    :new: Add Icon Support to Tab

--- Assets/JS/Tab.js
+++ Assets/JS/Tab.js
@@ -3,14 +3,19 @@
 "use strict";
 class Tab{
   constructor(URL){
+    let Title = document.createElement('span');
+    let Icon = document.createElement('span');
+
     this.Tab = document.createElement('paper-tab');
     this.WebView = document.createElement('webview');
 
-    this.Tab.textContent = 'New Tab';
+    Title.textContent = 'New Tab';
+    this.Tab.appendChild(Icon);
+    this.Tab.appendChild(Title);
 
     this.WebView.addEventListener('page-title-set', function(Event){
-      this.Tab.textContent = Event.title || 'New Tab';
-    }.bind(this));
+      Title.textContent = Event.title || 'New Tab';
+    });
 
     Main.TaskBar.Tabs.insertBefore(this.Tab, Main.TaskBar.Tabs.querySelector('paper-tab:last-child'));
     Main.Tabs.Root.appendChild(this.WebView);
