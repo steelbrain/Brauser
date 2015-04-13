4b5e9e6a17f7d3c627ed26aedfcad7c4d228a877
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-13 15:30:23 -0700

    :new: Add Tabs Class

---
+++ Assets/JS/Tabs.js
@@ -0,0 +1,26 @@
+
+
+"use strict";
+class Tabs{
+  constructor(){
+    this.Active = null;
+    this.Tabs = [];
+    this.Root = $("div[data-role=Content]");
+  }
+  CreateNew(Link, IsSelected){
+    let TheTab = new Tab(Link);
+    this.Tabs.push(TheTab);
+    if(IsSelected){
+      this.Select(TheTab);
+    }
+    return TheTab;
+  }
+  Select(Tab){
+    if(this.Active){
+      this.Active.WebView.classList.remove('show');
+    }
+    Main.TaskBar.Tabs.selected = this.Tabs.indexOf(Tab);
+    this.Active = Tab;
+    this.Active.WebView.classList.add('show');
+  }
+}
