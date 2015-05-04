1831f2b9a1d0ad22fd4b699f014309c84bcc4990
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-04 05:38:53 -0700

    :art: Improve Tab Event Handling

--- Assets/JS/Tab.js
+++ Assets/JS/Tab.js
@@ -1,8 +1,16 @@
 
 
 "use strict";
-class Tab{
+var NavStatus = {
+  START: 'START',
+  STOP: 'STOP',
+  REDIRECT: 'REDIRECT',
+  FAIL: 'FAIL'
+};
+class Tab extends EventEmitter{
   constructor(URL){
+    super();
+    let Emit = this.emit.bind(this);
     let Title = document.createElement('span');
     let Icon = document.createElement('span');
 
@@ -10,6 +18,7 @@ class Tab{
     this.WebView = document.createElement('webview');
 
     Title.textContent = 'New Tab';
+
     this.Tab.appendChild(Icon);
     this.Tab.appendChild(Title);
 
@@ -19,20 +28,22 @@ class Tab{
     this.WebView.setAttribute('src', URL);
 
     this.WebView.addEventListener('page-title-set', function(Event){
-      Title.textContent = Event.title || 'New Tab';
+      Emit('Title:Update', Event.title);
     });
-
-    this.WebView.addEventListener('did-start-loading', this.OnURLUpdate.bind(this));
-    this.WebView.addEventListener('did-stop-loading', this.OnURLUpdate.bind(this));
-    this.WebView.addEventListener('did-get-redirect-request', function(Event){
-      if(Event.isMainFrame)
-        this.OnURLUpdate();
-    }.bind(this));
-  }
-  OnURLUpdate(){
-    if(Main.Tabs.Active !== this){
-      return ;
-    }
-    Main.URLBar.URL.value = this.WebView.getUrl();
+    this.WebView.addEventListener('did-start-loading', function(){
+      Emit('Navigation:Status', NavStatus.START)
+    });
+    this.WebView.addEventListener('did-stop-loading', function(){
+      Emit('Navigation:Status', NavStatus.STOP);
+    });
+    this.WebView.addEventListener('did-finish-loading', function(){
+      Emit('Navigation:Status', NavStatus.STOP);
+    });
+    this.WebView.addEventListener('did-fail-load', function(Event){
+      Emit('Navigation:Status', NavStatus.FAIL, {code: Event.errorCode, description: Event.errorDescription});
+    });
+    this.WebView.addEventListener('page-favicon-updated', function(Event){
+      Emit('Favicon:Update', Event.favicons[0]);
+    });
   }
 }
