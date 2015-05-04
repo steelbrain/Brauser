806f32ceb9f7824af25cb494cf97be99d3bf67bd
Steel Brain <aneesiqbalbhatti@gmail.com>
2015-05-04 05:39:51 -0700

    :art: Point Package to Main.js directly We don't need Starter.js because Since Chrome v42, Classes are supported natively

--- Browser/Starter.js
+++
@@ -1,7 +0,0 @@
-
-
-var v8 = require('v8');
-v8.setFlagsFromString('--harmony_classes');
-v8.setFlagsFromString('--harmony_object_literals');
-v8.setFlagsFromString('--harmony_tostring');
-require('./Main');
